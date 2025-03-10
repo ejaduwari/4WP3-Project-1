b26cb3823d19fa610849ce9dda49160cbd2fff68
Your Name <aduawari88@gmail.com>
2025-03-10 13:14:17 -0400

    first com

---
+++ control.js
@@ -0,0 +1,47 @@
+const express = require("express");
+
+const mustacheExpress = require("mustache-express");
+const app = express();
+
+const Model = require("./model.js"); // include/import our model
+
+app.engine("mustache", mustacheExpress());
+app.set("view engine", "mustache");
+app.set("views", __dirname + "/views");
+
+async function start(){
+    await Model.makeConnection();
+}
+start();
+
+app.get("/selectExercise", async function(req, res){
+    //await Model.selectExercise(req.query);
+    const selected = await Model.selectExercise(req.query);
+    const exerciseArray = await Model.getAll();
+    res.render("page", { options: exerciseArray });
+    console.log(selected[0].Exercise);
+});
+
+app.get("/addExercise", async function(req, res){
+    await Model.addExercise(req.query);
+    const exerciseArray = await Model.getAll();
+    res.render("page", { options: exerciseArray });
+});
+
+app.get("/newExercise", async function(req, res){
+    const exerciseArray = await Model.getAll();
+    res.render("page", { options: exerciseArray, addExercise: true });
+});
+
+app.get("/page", async function(req, res){
+    const exerciseArray = await Model.getAll();
+    //console.log(exerciseArray.name);
+    res.render("page", { options: exerciseArray });
+});
+
+app.get(/^(.+)$/, function(req,res) 
+{
+  res.sendFile( __dirname + req.params[0]);
+});
+
+app.listen(3000, function() { console.log("server listening on port 3000...")});
---
+++ model.js
@@ -0,0 +1,35 @@
+const sqlite3 = require("sqlite3").verbose();
+const sqlite = require("sqlite");
+
+let db;
+
+async function makeConnection()
+{
+  db = await sqlite.open({
+    filename: 'gym.db',
+    driver: sqlite3.Database
+  })
+};
+
+async function selectExercise(variable)
+{
+    //console.log(variable); // use this to ensure we are passing the right data
+    const results = await db.all("SELECT * FROM Exercises WHERE Exercise = ?", [variable.selected]);
+    //console.log(results); // use this to ensure we get back the right data
+    return results;
+}
+
+async function getAll()
+{
+    const results = await db.all("SELECT * FROM Exercises");
+    return results;
+}
+
+async function addExercise(exercise)
+{
+    await db.run("INSERT INTO Exercises VALUES (?)", [exercise.name]);
+}
+
+
+
+module.exports = { makeConnection, getAll, addExercise, selectExercise };
